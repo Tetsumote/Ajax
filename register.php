8738103b9a30529964dd684dc3aabf2e140216b8
Tetsumote <unionproject88@gmail.com>
2017-05-11 08:50:23 +0200

    com.16 Ajax POST form feedback

---
+++ register.php
@@ -0,0 +1,4 @@
+<?php
+echo "Dziękujemy za dołączenie się, " . $_POST["name"] . "!<br>";
+echo "Wiadomość e-mail z potwierdzenie została wysłana na adres: " . $_POST["email"] . ", gdy to była rzeczywista witryna.";
+?>
