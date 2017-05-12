1eb8ac6a4142d69dea09a85793c0134bd57bc347
Tetsumote <unionproject88@gmail.com>
2017-05-12 02:55:10 +0200

    com.17 Ajax loading HTML

---
+++ js/AJAX.js
@@ -0,0 +1,28 @@
+$('nav a').on('click', function(e) {
+  e.preventDefault();
+  var url = this.href;                                     
+  var $content = $('#content');                            
+
+  $('nav a.current').removeClass('current');               
+  $(this).addClass('current');
+  $('#container').remove();                                 
+
+  $.ajax({
+    type: "POST",                                           
+    url: url,                                               
+    timeout: 2000,                                          
+    beforeSend: function() {                                
+      $content.append('<div id="load">Wczytywanie</div>');  
+    },
+    complete: function() {                                  
+      $('#loading').remove();                               
+    },
+    success: function(data) {                               
+      $content.html( $(data).find('#container') ).hide().fadeIn(400);
+    },
+    fail: function() {                                       
+      $('#panel').html('<div class="loading">Proszę spróbować wkrótce.</div>');
+    }
+  });
+
+});
